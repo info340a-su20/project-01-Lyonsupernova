f1905e9c71815a8b37649758067d0d45160bfab7
WayneXYZ <waynescrown@gmail.com>
2020-08-21 23:59:40 -0500

    final modifications

--- js/landing-page.js
+++ js/landing-page.js
@@ -62,5 +62,4 @@ jQuery(document).ready(function($) {
   
 });
 
-
-TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
+// TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
